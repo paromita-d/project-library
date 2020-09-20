ca118825781f5581237668000f56dc876c09500a
paromita-d <paromita.newid@gmail.com>
2020-09-20 11:19:59 -0400

    added Repository, Tests for metadata

--- library-be/src/main/java/org/library/dto/MetaData.java
+++ library-be/src/main/java/org/library/dto/MetaData.java
@@ -1,10 +1,20 @@
 package org.library.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data @Builder
+import javax.persistence.*;
+
+@Entity @Table(name = "metadata")
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class MetaData {
+
+    @Id
+    @Column(name = "meta_key")
     private String metaKey;
+
+    @Column(name = "meta_value")
     private String metaValue;
 }
